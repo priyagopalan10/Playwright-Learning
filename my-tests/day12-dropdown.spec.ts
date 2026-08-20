5ab05987dbff90f57c0e0ec5f5faa3f35fddb2da
Gopalan <gopalanp@amazon.com>
2026-08-20 15:24:08 +0530

    Add Day 12 dropdown sorting test

---
+++ my-tests/day12-dropdown.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('Select product sort option', async ({ page }) => {
+
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await page.locator('[data-test="product-sort-container"]').selectOption('lohi');
+await expect(
+    page.locator('[data-test="product-sort-container"]')
+).toHaveValue('lohi');
+const prices = page.locator('.inventory_item_price');
+const pricesT = await prices.allTextContents();
+console.log(pricesT);
+await expect(prices.first()).toHaveText('$7.99');
+});
