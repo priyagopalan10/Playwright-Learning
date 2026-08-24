6f8af8e9ceeb0a10ece5b2b15c85ffe9a990ecbf
Gopalan <gopalanp@amazon.com>
2026-08-24 17:32:34 +0530

    Update Day 13 role locator practice

--- my-tests/day13-role-locator.spec.ts
+++ my-tests/day13-role-locator.spec.ts
@@ -1,13 +1,56 @@
-import { test, expect } from '@playwright/test'; 
- 
-test('Role of locator', async ({ page }) => { 
- 
+import { test, expect } from '@playwright/test';
+
+test('Day 13 - Verify Backpack Add to Cart button', async ({ page }) => {
+
     await page.goto('https://www.saucedemo.com/');
 
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('secret_sauce');
     await page.getByRole('button', { name: 'Login' }).click();
-const backpack = page.locator('.inventory_item'). filter({hasText : 'Sauce Labs Backpack'});
-const addToCart = backpack.getByRole('button', { name : 'Add to cart'});
-await expect(addToCart). toBeVisible();
+
+    const backpack = page.locator('.inventory_item')
+        .filter({ hasText: 'Sauce Labs Backpack' });
+
+    const addToCart = backpack.getByRole('button', {
+        name: 'Add to cart',
+        exact: true
+    });
+
+    await expect(addToCart).toBeVisible();
+});
+
+
+test('Day 13 - Verify and click Bike Light Add to Cart', async ({ page }) => {
+
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    const bikelight = page.locator('.inventory_item')
+        .filter({ hasText: 'Sauce Labs Bike Light' });
+
+    const addToCart = bikelight.getByRole('button', {
+        name: 'Add to cart',
+        exact: true
+    });
+
+    await expect(addToCart).toBeVisible();
+
+    await addToCart.click();
+});
+
+
+test('Day 13 - Login using labels', async ({ page }) => {
+
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.getByLabel('Username').fill('standard_user');
+    await page.getByLabel('Password').fill('secret_sauce');
+
+    await page.getByRole('button', {
+        name: 'Login',
+        exact: true
+    }).click();
 });
